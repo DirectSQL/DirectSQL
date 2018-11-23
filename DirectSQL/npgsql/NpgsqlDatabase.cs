a494aa092b01203f1c8572d960a473850ebbf14a
7k8m <nkym.tmht@gmail.com>
2018-11-23 17:06:02 +0900

    Code for Npgsql is added

---
+++ DirectSQL/npgsql/NpgsqlDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+using Npgsql;
+
+namespace DirectSQL.Npgsql
+{
+    public class NpgsqlDatabase : Database
+    {
+
+        private String _npgsqlConnectionString;
+
+        public NpgsqlDatabase( String npgsqlConnectionString)
+        {
+            _npgsqlConnectionString = npgsqlConnectionString;
+        }
+
+        protected override IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection( _npgsqlConnectionString );
+        }
+
+
+        public static NpgsqlParameter CreateSQLiteParameter(string name, object value)
+        {
+            return new NpgsqlParameter(name, value);
+        }
+
+    }
+}
