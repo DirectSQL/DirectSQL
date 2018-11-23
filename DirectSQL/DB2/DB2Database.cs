274b0b169362226b096916bfd7bcf71759e46a1a
7k8m <nkym.tmht@gmail.com>
2018-11-23 19:15:35 +0900

    Module for DB2 is added.

---
+++ DirectSQL/DB2/DB2Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+using IBM.Data.DB2.Core;
+
+namespace DirectSQL.DB2
+{
+    public class DB2Database : Database
+    {
+
+        readonly private String _db2ConnectionString;
+
+        public DB2Database(String db2ConnectionString)
+        {
+            _db2ConnectionString = db2ConnectionString;
+        }
+
+        protected override IDbConnection CreateConnection()
+        {
+            return new DB2Connection ( _db2ConnectionString );
+        }
+
+        public static DB2Parameter CreateDB2Parameter(string name, object value)
+        {
+            return new DB2Parameter(name, value);
+        }
+
+    }
+}
