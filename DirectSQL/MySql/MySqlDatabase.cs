eef5fd93f165321877e6255ceb03e1b44aa80b80
7k8m <nkym.tmht@gmail.com>
2018-11-24 09:18:44 +0900

    Added module for mysql

---
+++ DirectSQL/MySql/MySqlDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+using MySql.Data.MySqlClient;
+
+namespace DirectSQL.MySql
+{
+    public class MySqlDatabase : Database
+    {
+
+        readonly private String _mySqlConnectionString;
+
+        public MySqlDatabase(String mySqlConnectionString)
+        {
+            _mySqlConnectionString = mySqlConnectionString;
+        }
+
+        protected override IDbConnection CreateConnection()
+        {
+            return new MySqlConnection( _mySqlConnectionString );
+        }
+
+
+        public static MySqlParameter CreateMySqlParameter(string name, object value)
+        {
+            return new MySqlParameter(name, value);
+        }
+
+    }
+}
