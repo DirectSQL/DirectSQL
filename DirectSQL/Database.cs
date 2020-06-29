548c0b15ccce6a7872bb32662b5a110faf7d87a5
Tomohito Nakayama <nkym.tmht@gmail.com>
2020-06-29 21:26:00 +0900

    Delete missed Database.DefaultTransaction

--- DirectSQL/Database.cs
+++ DirectSQL/Database.cs
@@ -894,38 +894,5 @@ namespace DirectSQL
             {
             }
         }
-
-        /// <summary>
-        /// Marker object indicates default transaction is used.
-        /// </summary>
-        internal class DefaultTransaction : IDbTransaction
-        {
-            /// <summary>
-            /// Instance of DefaultTransaction
-            /// </summary>
-            internal static readonly DefaultTransaction defaultTransaction = new DefaultTransaction();
-
-            private DefaultTransaction()
-            {
-            }
-
-            public void Dispose()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Commit()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Rollback()
-            {
-                throw new NotImplementedException();
-            }
-
-            public IDbConnection Connection { get; }
-            public IsolationLevel IsolationLevel { get; }
-        }
     }
 }
