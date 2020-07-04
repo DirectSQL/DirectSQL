a95166427e5f92a8d33466d82d1009d00c805c44
Tomohito Nakayama <nkym.tmht@gmail.com>
2020-07-05 00:16:23 +0900

    Use returned Database in ConnectionTest

--- TestSqlLiteDatabase/ConnectionTest.cs
+++ TestSqlLiteDatabase/ConnectionTest.cs
@@ -15,30 +15,26 @@ namespace TestSqlLiteDatabase
         {
             SqlLiteDatabase db = new SqlLiteDatabase("Data Source=:memory:");
 
-            db.Process((connection) => {
-            });
-
-            db.Process( (connection, transaction) =>
-            {
-            });
+            db
+            .Process((connection) => {})
+            .Process((connection, transaction) => {} );
         }
 
         [TestMethod]
-        public void TestConnectAsync()
+        public async Task TestConnectAsync()
         {
             SqlLiteDatabase db = new SqlLiteDatabase("Data Source=:memory:");
 
-            Task task1 = db.ProcessAsync(async (connection) =>
-            {
-                await Task.Delay(1); //Dummy code to resolve warning in build.
+            await 
+            ( await db.ProcessAsync(
+                async (connection) => {
+                    await Task.Delay(1); //Dummy code to resolve warning in build.
+                })
+            )
+            .ProcessAsync( 
+                async (connection, transaction) =>{
+                    await Task.Delay(1); //Dummy code to resolve warning in build.
             });
-            task1.Wait();
-
-            Task task2 = db.ProcessAsync( async (connection, transaction) =>
-            {
-                await Task.Delay(1); //Dummy code to resolve warning in build.
-            });
-            task2.Wait();
         }
     }
 }
